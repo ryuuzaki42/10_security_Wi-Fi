51a7ed1c878e40e4e2f153294ebc85980bddc0a2
João Batista <joao42lbatista@gmail.com>
2016-10-08 16:16:21 -0300

    Adding programas.r

---
+++ programas.r
@@ -0,0 +1,25 @@
+    ## programas
+aircrack-ng
+
+fing
+
+mmap
+
+zemmap
+
+wireshark
+
+    # John the Ripper
+jonh
+
+kismet
+
+iwlist wlan0 scan
+
+iw dev wlan0 link
+
+ping -c 3 google.com
+
+nslookup google.com
+
+nslookup -query=AAAA 2a03:2880:f000:1:face:b00c:0:1
