d4326809c4a42902ffad8f9a913ea5d57573ff6f
João Batista <joao42lbatista@gmail.com>
2016-10-08 16:23:32 -0300

    Adding http/s decrip infos

---
+++ pacotes capturados/http_https/http_https_decrip.r
@@ -0,0 +1,30 @@
+    ## http x https
+wireshark
+
+## http
+http://unionmangas.net/
+
+## https
+https://www.facebook.com/
+
+## site with https "grátis"
+https://saikoanimes.net/
+## CA "grátis"
+https://letsencrypt.org/
+
+## WPA2 desciptografia
+    ## Necessário capturar os 4-way handshake (EAPoL)
+
+    ## Abra o arquivo cap no wireshark (wpa2_decrip.cap)
+
+    ## É preciso a chave PSK ou nome da rede e o senha para decriptografar os dados
+    
+    ## PSK da rede capturada: 
+    d494a04654c0922aaed4ea24facc9903b03820149d79e4fe9ce7e8fb35fea3f0
+    
+    ## Adicione ela no wireshark
+    ​Edit -> Preferences -> IEEE 802.11
+    ​
+    ​## Marque Enable decryption
+    
+    ## Aplique o filtro http no wireshark
