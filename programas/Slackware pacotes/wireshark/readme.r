116e4b1066fdbfdfb9a47444db3cef0b418f17f4
João Batista <joao42lbatista@gmail.com>
2017-09-04 11:09:02 -0300

    wireshark x86_64 - update "2.4.0" to "2.4.1"

--- programas/Slackware pacotes/wireshark/readme.r
+++ programas/Slackware pacotes/wireshark/readme.r
@@ -1,7 +1,7 @@
 ## Wireshark informations
-    Wireshark (aka Ethereal) is a free packet sniffer computer application.  It 
+    Wireshark (aka Ethereal) is a free packet sniffer computer application.  It
     is used for network troubleshooting, analysis, software and communications
-    protocol development, and education.  In June 2006, the project was renamed 
+    protocol development, and education.  In June 2006, the project was renamed
     from Ethereal due to trademark issues.
 
     The functionality Wireshark provides is very similar to tcpdump, but it has
@@ -14,14 +14,14 @@
     make it the tool of choice for network troubleshooting, protocol development,
     and education worldwide.
 
-    If you use a filesystem that supports posix capabilities, an easy way to 
-    start wireshark as a normal user, while still providing it with all of the 
+    If you use a filesystem that supports posix capabilities, an easy way to
+    start wireshark as a normal user, while still providing it with all of the
     access permissions it requires, is by issuing the following command:
     $ setcap cap_net_raw,cap_net_admin=eip /usr/bin/dumpcap
 
-    You will need to remove any already-installed wireshark package before 
+    You will need to remove any already-installed wireshark package before
     building this one or else the new one will not work (the new build will
-    link libraries present in the old package, which will then be removed 
+    link libraries present in the old package, which will then be removed
     when upgrading).
 
 ## To build with GTK add "--with-gtk" in the ./configure command
