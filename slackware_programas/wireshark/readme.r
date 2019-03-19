bb014040c3028bafce5d3c33860031dae887b61c
João D. Batista <joao42lbatista@gmail.com>
2019-03-19 14:44:27 -0300

    readme.r - Wireshark 3.0 no longer support legacy GTK+ UI

--- slackware_programas/wireshark/readme.r
+++ slackware_programas/wireshark/readme.r
@@ -10,7 +10,7 @@
     (usually an Ethernet network but support is being added for others) by
     putting the network interface into promiscuous mode.
 
-    Wireshark uses the cross-platform GTK+ widget toolkit.  Its powerful features
+    Wireshark uses the cross-platform Qt5 widget toolkit.  Its powerful features
     make it the tool of choice for network troubleshooting, protocol development,
     and education worldwide.
 
@@ -23,6 +23,24 @@
     building this one or else the new one will not work (the new build will
     link libraries present in the old package, which will then be removed
     when upgrading).
+
+    Optional dependencies:
+    - c-ares
+    - krb5
+    - nghttp2
+    - spandsp
+    - snappy
+    - lz4
+    - libsmi
+
+    NOTE: Wireshark 3.0 no longer support legacy GTK+ UI.
+
+    NOTE: Although lua is listed as a requirement, it's possible to build
+    using lua52. This may be needed for some third-party Lua plugins (but,
+    it may break other third-party Lua plugins; your mileage may vary). The
+    default is to build with lua52 if it's installed, or lua otherwise. If
+    you have both lua versions installed, you can build with LUA52=no in
+    the environment to build with the older lua.
 
 ## To build with GTK add "--with-gtk" in the ./configure command
     ./configure \
