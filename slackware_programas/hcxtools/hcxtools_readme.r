ffa104ff5df6bb6d5f7a14057dd83b90a3486772
João D. Batista <joao42lbatista@gmail.com>
2020-02-09 21:56:24 -0300

    hcxtools-5.3.0-x86_84-JB.txz and hcxtools_readme.r - add

---
+++ slackware_programas/hcxtools/hcxtools_readme.r
@@ -0,0 +1,6 @@
+
+hcxpcaptool - For converting PMKID packet captures into hashcat's format.
+
+Small set of tools convert packets from captures for the use with latest hashcat or John the Ripper.
+
+https://github.com/ZerBea/hcxtools
